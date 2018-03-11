eec7c50de42bbc531304dda08caad7bcf1fa6827
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2018-03-12 07:14:09 +0900

    Update

--- R/Features.R
+++ R/Features.R
@@ -64,7 +64,7 @@ Features_PeptDesc <- function(
   # Start calculation
   set.seed(12345)
   time.start <- proc.time()
-
+  
   # Working functions
   peptideDescriptor.Batch <- function(peptide){
     unlist(list(
@@ -97,7 +97,7 @@ Features_PeptDesc <- function(
                "Mean"=matrixStats::rowMeans2(d),
                "Median"=matrixStats::rowMedians(d))
   }
-
+  
   # Parallelized calculation of descriptive statistics
   parameterGrid <- expand.grid(peptideSet, fragLenSet, stringsAsFactors=F)
   cl <- parallel::makeCluster(parallel::detectCores(), type="SOCK")
@@ -113,7 +113,7 @@ Features_PeptDesc <- function(
   )
   parallel::stopCluster(cl)
   gc();gc();
-
+  
   # Formatting
   df_feature <- data.table::rbindlist(df_feature) %>%
     tidyr::gather(Stat, Value, -Peptide, -FragLen, -AADescriptor) %>%
@@ -121,7 +121,7 @@ Features_PeptDesc <- function(
     tidyr::spread(Feature, Value)
   colnames(df_feature) <- paste0("PeptDesc_", colnames(df_feature))
   colnames(df_feature)[1] <- "Peptide"
-
+  
   # Basic peptide information
   df_basic <- data.frame("Peptide"=peptideSet, "Peptide_Length"=nchar(peptideSet))
   for(aa in Biostrings::AA_STANDARD){
@@ -129,7 +129,7 @@ Features_PeptDesc <- function(
   }
   df_feature <- suppressWarnings(dplyr::left_join(df_basic, df_feature, by="Peptide"))
   rownames(df_feature) <- 1:nrow(df_feature)
-
+  
   # Output
   time.end <- proc.time()
   message("Overall time required = ", format((time.end-time.start)[3], nsmall=2), "[sec]")
@@ -150,11 +150,11 @@ Features_CPP <- function(
 ){
   # Temp directory check
   dir.create(tmpDir, showWarnings=F, recursive=T)
-
+  
   # Start calculation
   set.seed(12345)
   time.start <- proc.time()
-
+  
   # Pairwise contact potential matrix
   AACPMatrix <- CPP_AACPMatrix()
   if(identical(aaIndexIDSet, "all")){
@@ -175,7 +175,7 @@ Features_CPP <- function(
       descriptorfile=AACPMatrix_descfile
     )
   }
-
+  
   # Fragment library for matching
   FragLib_TCR <- fst::read_fst(system.file("TCRFragmentLibrary.fst", package="Repitope"), as.data.table=T) ## fragmentLibrary(TCRSet_Public, 3:8)
   FragLib_TCR <- FragLib_TCR[FragmentLength %in% fragLenSet,]
@@ -191,7 +191,7 @@ Features_CPP <- function(
       descriptorfile=FragLib_descfile
     )
   }
-
+  
   # Contact potential profiling
   ## Working functions
   load_AACPMatrix <- function(aaIndexID){
@@ -220,9 +220,8 @@ Features_CPP <- function(
       scoreOnly=T
     )
     statSet <- c("mean","sd","median","trimmed","mad","skew","kurtosis","se","IQR","Q0.1","Q0.9")
-    statDF <- psych::describe(al, trim=.1, interp=F, skew=T, type=3, ranges=T, IQR=T, quant=c(.10, .90))[statSet]
-    colnames(statDF) <- c("Mean","SD","Med","TrM","MAD","Skew","Kurt","SE","IQR","Q10","Q90")
-    return(statDF)
+    stat <- psych::describe(al, trim=.1, interp=F, skew=T, type=3, ranges=T, IQR=T, quant=c(.10, .90))[statSet]
+    return(as.numeric(stat))
   }
   contact_potential_profiling <- function(peptideSet, aaIndexID, fragLen, fragDepth, seed, cl=cl){
     parallel::clusterExport(
@@ -237,17 +236,19 @@ Features_CPP <- function(
         set.seed(seed)
         AACPMatrix_Tmp <- load_AACPMatrix(aaIndexID)
         FragLib_Tmp <- load_FragLib(fragLen, fragDepth, seed)
-        df_meta <- data.frame("Peptide"=pept, "AAIndexID"=aaIndexID, "FragLen"=fragLen, "FragDepth"=fragDepth, "Seed"=seed, stringsAsFactors=F)
-        df <- data.table::rbindlist(lapply(FragLib_Tmp, function(d){cbind(df_meta, calculate_stats_single(pept, d, AACPMatrix_Tmp, seed))}))
+        df <- data.table::transpose(
+          as.data.frame(sapply(FragLib_Tmp, function(d){calculate_stats_single(pept, d, AACPMatrix_Tmp, seed)}))
+        )
         df$"Library" <- c("Deduplicated", "Weighted", "Mock")
         return(df)
       }
     )
     dt <- data.table::as.data.table(data.table::rbindlist(dt))
-    data.table::setcolorder(dt, c("Peptide", "AAIndexID", "FragLen", "FragDepth", "Library", "Seed", "Mean","SD","Med","TrM","MAD","Skew","Kurt","SE","IQR","Q10","Q90"))
+    colnames(dt) <- c("Mean","SD","Med","TrM","MAD","Skew","Kurt","SE","IQR","Q10","Q90","Library")
+    dt[,"Peptide":=peptideSet]
     return(dt)
   }
-
+  
   ## Main
   parameterGrid <- expand.grid(aaIndexIDSet, fragLenSet, fragDepthSet, seedSet, stringsAsFactors=F) %>%
     magrittr::set_colnames(c("AAIndexID","FragLen","FragDepth","Seed"))
@@ -276,23 +277,24 @@ Features_CPP <- function(
     }
   )
   parallel::stopCluster(cl)
-
+  
   ## Final formatting
   message("Data formatting...")
   dt_cpp <- pbapply::pblapply(
     list.files(pattern="^dt_feature_cpp.+fst$", path=tmpDir, full.names=T), fst::read_fst, as.data.table=T
   ) %>% data.table::rbindlist() %>% data.table::as.data.table()
+  dt_cpp <- data.table::as.data.table(cbind(parameterGrid, dt_cpp))
   data.table::setorder(dt_cpp, Peptide, AAIndexID, FragLen, FragDepth, Library, Seed)
   col_id <- c("Peptide", "AAIndexID", "FragLen", "FragDepth", "Library", "Seed")
   col_val <- setdiff(colnames(dt_cpp), col_id)
   dt_cpp <- data.table::melt.data.table(dt_cpp, id=col_id, measure=col_val, variable.name="Stat", value.name="Value")
   dt_cpp[,"Feature":=paste0("CPP_", AAIndexID, "_", Stat, "_", FragLen)][,"AAIndexID":=NULL][,"FragLen":=NULL][,"Stat":=NULL]
   dt_cpp <- data.table::dcast.data.table(dt_cpp, Peptide+FragDepth+Library~Feature, value.var="Value", fun.aggregate=mean)
-
+  
   # Finish the timer
   time.end <- proc.time()
   message("Overall time required = ", format((time.end-time.start)[3], nsmall=2), "[sec]")
-
+  
   # Clear logs
   message("Erase the temporary folder...")
   rm(list=setdiff(ls(), c("tmpDir", "AACPMatrix_binfile", "FragLib_binfile", "dt_cpp")))
@@ -300,7 +302,7 @@ Features_CPP <- function(
   file.remove(file.path(tmpDir, AACPMatrix_binfile))
   file.remove(file.path(tmpDir, FragLib_binfile))
   file.remove(list.files(pattern="^dt_feature_cpp.+fst$", path=tmpDir, full.names=T))
-
+  
   # Output
   return(dt_cpp)
 }
