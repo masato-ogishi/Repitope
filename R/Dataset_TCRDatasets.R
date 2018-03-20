755c911313f053c001237979792c46bbcf6f7169
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2018-03-21 08:40:29 +0900

    Minor update

--- R/Dataset_TCRDatasets.R
+++ R/Dataset_TCRDatasets.R
@@ -1,6 +1,6 @@
-#' Compiled dataset of human TCR beta CDR3 sequences
+#' A compiled dataset of human TCR beta CDR3 sequences
 #'
-#' \code{TCRSet_Public} is a set of human TCR beta CDR3 clonotype sequences observed in at least 22 out of 218 datasets.
+#' \code{TCRSet_Public} is a set of human TCR beta CDR3 clonotype sequences observed in at least 22 out of 220 datasets.
 #'
 #' @format A character vector containing 191,326 sequences.
 #' @docType data
