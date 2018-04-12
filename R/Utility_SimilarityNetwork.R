44842c7adfb673d3d1270ea46b18789d8d049c59
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2018-04-12 19:02:09 +0900

    Bug fix

--- R/Utility_SimilarityNetwork.R
+++ R/Utility_SimilarityNetwork.R
@@ -1,8 +1,8 @@
 #' Similarity network analysis.
 #'
-#' @param aaStringSet A set of sequences. Simple character strings will be internally converted to an AAStringSet object.
-#' @param longerAAStringSet A set of sequences. Should be one amino acid longer than \code{shorterAAStringSet}.
-#' @param shorterAAStringSet A set of sequences. Should be one amino acid shorter than \code{longerAAStringSet}.
+#' @param peptideSet A set of peptide sequences.
+#' @param longerPeptideSet A set of peptide sequences. Should be one amino acid longer than \code{shorterPeptideSet}.
+#' @param shorterPeptideSet A set of peptide sequences. Should be one amino acid shorter than \code{longerPeptideSet}.
 #' @param numSet An attribute for the vertices.
 #' @param directed Should the network be converted from undirected to directed? Directions are determined by the \code{numSet} provided.
 #' @param weighted Should the network be converted to weihted? Edge weights are determined by the \code{numSet} provided.
@@ -38,47 +38,47 @@
 #' @export
 #' @rdname Utility_SimilarityNetwork
 #' @name Utility_SimilarityNetwork
-distMat_Auto <- function(aaStringSet){
+distMat_Auto <- function(peptideSet){
   # Input check...
-  if(class(aaStringSet)!="AAStringSet"){
-    aaStringSet <- Biostrings::AAStringSet(aaStringSet)
+  if(class(peptideSet)!="AAStringSet"){
+    peptideSet <- Biostrings::AAStringSet(peptideSet)
   }
-  if(length(unique(S4Vectors::nchar(aaStringSet)))>=2){
+  if(length(unique(S4Vectors::nchar(peptideSet)))>=2){
     message("Input sequences must be of the same length!")
     return(NULL)
   }
 
-  sequenceSet <- as.character(aaStringSet)
-  DistMat.auto <- Matrix::sparseMatrix(c(), c(), x=F, dims=c(length(aaStringSet), length(aaStringSet)))
-  pbQ <- length(aaStringSet)>1000
+  sequenceSet <- as.character(peptideSet)
+  DistMat.auto <- Matrix::sparseMatrix(c(), c(), x=F, dims=c(length(peptideSet), length(peptideSet)))
+  pbQ <- length(peptideSet)>1000
   if(pbQ){
-    pb <- pbapply::timerProgressBar(min=1, max=length(aaStringSet), char="+", style=1)
-    for(i in 1:length(aaStringSet)){
+    pb <- pbapply::timerProgressBar(min=1, max=length(peptideSet), char="+", style=1)
+    for(i in 1:length(peptideSet)){
       pbapply::setTimerProgressBar(pb, i)
-      DistMat.auto[i, 1:i] <- stringdist::stringdist(sequenceSet[[i]], aaStringSet[1:i], method="hamming")<=1
+      DistMat.auto[i, 1:i] <- stringdist::stringdist(sequenceSet[[i]], peptideSet[1:i], method="hamming")<=1
     }
     cat("\n")
   }else{
-    for(i in 1:length(aaStringSet)){
-      DistMat.auto[i, 1:i] <- stringdist::stringdist(sequenceSet[[i]], aaStringSet[1:i], method="hamming")<=1
+    for(i in 1:length(peptideSet)){
+      DistMat.auto[i, 1:i] <- stringdist::stringdist(sequenceSet[[i]], peptideSet[1:i], method="hamming")<=1
     }
   }
   diag(DistMat.auto) <- F
-  return(DistMat.auto)  # An m x m triangular matrix: m = length(aaStringSet)
+  return(DistMat.auto)  # An m x m triangular matrix: m = length(peptideSet)
 }
 #' @export
 #' @rdname Utility_SimilarityNetwork
 #' @name Utility_SimilarityNetwork
-distMat_Juxtaposed <- function(longerAAStringSet, shorterAAStringSet){
+distMat_Juxtaposed <- function(longerPeptideSet, shorterPeptideSet){
   # Input check...
-  if(class(longerAAStringSet)!="AAStringSet"){
-    longerAAStringSet <- Biostrings::AAStringSet(longerAAStringSet)
+  if(class(longerPeptideSet)!="AAStringSet"){
+    longerPeptideSet <- Biostrings::AAStringSet(longerPeptideSet)
   }
-  if(class(shorterAAStringSet)!="AAStringSet"){
-    shorterAAStringSet <- Biostrings::AAStringSet(shorterAAStringSet)
+  if(class(shorterPeptideSet)!="AAStringSet"){
+    shorterPeptideSet <- Biostrings::AAStringSet(shorterPeptideSet)
   }
-  longerSeq <- as.character(longerAAStringSet)
-  shorterSeq <- as.character(shorterAAStringSet)
+  longerSeq <- as.character(longerPeptideSet)
+  shorterSeq <- as.character(shorterPeptideSet)
   leng_longer <- unique(nchar(longerSeq))
   leng_shorter <- unique(nchar(shorterSeq))
   if(length(leng_longer)>=2){
@@ -100,40 +100,40 @@ distMat_Juxtaposed <- function(longerAAStringSet, shorterAAStringSet){
 
   longerSeq.degenerate <- unlist(lapply(1:leng_longer, function(i){seq <- longerSeq; stringr::str_sub(seq, i, i) <- ""; return(seq)}))
   longerSeq.degenerate <- matrix(longerSeq.degenerate, ncol=leng_longer)
-  DistMat.juxt <- Matrix::sparseMatrix(c(), c(), x=F, dims=c(length(shorterAAStringSet), length(longerAAStringSet)))
-  pbQ <- length(shorterAAStringSet)>1000
+  DistMat.juxt <- Matrix::sparseMatrix(c(), c(), x=F, dims=c(length(shorterPeptideSet), length(longerPeptideSet)))
+  pbQ <- length(shorterPeptideSet)>1000
   if(pbQ){
-    pb <- pbapply::timerProgressBar(min=1, max=length(shorterAAStringSet), char="+", style=1)
-    for(i in 1:length(shorterAAStringSet)){
+    pb <- pbapply::timerProgressBar(min=1, max=length(shorterPeptideSet), char="+", style=1)
+    for(i in 1:length(shorterPeptideSet)){
       pbapply::setTimerProgressBar(pb, i)
       DistMat.juxt[i,] <- rowSums(longerSeq.degenerate==shorterSeq[[i]])!=0
     }
     cat("\n")
   }else{
-    for(i in 1:length(shorterAAStringSet)){
+    for(i in 1:length(shorterPeptideSet)){
       DistMat.juxt[i,] <- rowSums(longerSeq.degenerate==shorterSeq[[i]])!=0
     }
   }
   DistMat.juxt <- Matrix::t(DistMat.juxt)
-  return(DistMat.juxt) # An m x n matrix: m = length(longerAAStringSet), n = length(shorterAAStringSet)
+  return(DistMat.juxt) # An m x n matrix: m = length(longerPeptideSet), n = length(shorterPeptideSet)
 }
 #' @export
 #' @rdname Utility_SimilarityNetwork
 #' @name Utility_SimilarityNetwork
-singleAASimilarityNetwork <- function(aaStringSet, numSet=NULL, directed=T, weighted=T, forceMutType=F){
+singleAASimilarityNetwork <- function(peptideSet, numSet=NULL, directed=T, weighted=T, forceMutType=F){
   # Internally used workflows
-  net_main <- function(aaStringSet){
+  net_main <- function(peptideSet){
     ## Input check...
-    if(class(aaStringSet)!="AAStringSet"){
-      aaStringSet <- Biostrings::AAStringSet(aaStringSet)
+    if(class(peptideSet)!="AAStringSet"){
+      peptideSet <- Biostrings::AAStringSet(peptideSet)
     }
-    if(length(unique(aaStringSet))!=length(aaStringSet)){
+    if(length(unique(peptideSet))!=length(peptideSet)){
       message("Duplicates are removed from the input sequences.")
-      aaStringSet <- unique(aaStringSet)
+      peptideSet <- unique(peptideSet)
     }
 
     ## Serialized adjacency matrix calculation
-    aaStringSetList <- split(aaStringSet, S4Vectors::nchar(aaStringSet))
+    aaStringSetList <- split(peptideSet, S4Vectors::nchar(peptideSet))
     if(length(names(aaStringSetList))>=2){
       sequenceLengthPairGrid <- suppressWarnings(dplyr::bind_rows(
         data.frame(V1=names(aaStringSetList), V2=names(aaStringSetList), Type="Auto"),
@@ -146,12 +146,12 @@ singleAASimilarityNetwork <- function(aaStringSet, numSet=NULL, directed=T, weig
       dplyr::filter((as.numeric(V1)-as.numeric(V2)) %in% c(0, 1)) ## V1>=V2
     sequenceLengthPairN <- nrow(sequenceLengthPairGrid)
 
-    ends <- as.numeric(cumsum(table(S4Vectors::nchar(aaStringSet))))
+    ends <- as.numeric(cumsum(table(S4Vectors::nchar(peptideSet))))
     starts <- (c(0, ends)+1)[1:length(ends)]
     positionGrid <- as.data.frame(t(data.frame(starts, ends)))
     colnames(positionGrid) <- names(aaStringSetList)
 
-    DistMat <- Matrix::sparseMatrix(c(), c(), x=F, dims=rep(length(aaStringSet), 2))
+    DistMat <- Matrix::sparseMatrix(c(), c(), x=F, dims=rep(length(peptideSet), 2))
 
     message(paste0("Number of sequence length pairs = ", sequenceLengthPairN))
     for(i in which(sequenceLengthPairGrid$"Type"=="Auto")){
@@ -219,45 +219,48 @@ singleAASimilarityNetwork <- function(aaStringSet, numSet=NULL, directed=T, weig
         return(sbst)
       }
     }
+    mutPattern <- function(pept1, pept2){
+      if(nchar(pept1)==nchar(pept2)) return("Substitution")
+      if(nchar(pept1)<nchar(pept2)) return("Insertion")
+      if(nchar(pept1)>nchar(pept2)) return("Deletion")
+    }
     message("Annotating mutational types and patterns...")
     if(forceMutType==T|nrow(df)<=50000){
-      df[["MutType"]] <- unlist(pbapply::pblapply(1:nrow(df), function(i){mutType(df$"AASeq1"[i], df$"AASeq2"[i])}))
-    } ## a bit slow...
-    df[["MutPattern"]] <- dplyr::if_else(nchar(df$"AASeq1")==nchar(df$"AASeq2"), "Substitution", "Indel")
+      df[["MutType"]] <- unlist(pbapply::pblapply(1:nrow(df), function(i){mutType(df$"AASeq1"[[i]], df$"AASeq2"[[i]])})) ## a bit slow...
+    }else{
+      df[["MutType"]] <- NA
+    }
+    df[["MutPattern"]] <- unlist(pbapply::pblapply(1:nrow(df), function(i){mutType(df$"AASeq1"[[i]], df$"AASeq2"[[i]])}))
 
     ## Output
     return(df)
   }
 
   # A basic, non-directional, non-weighted network
-  simNet <- net_main(aaStringSet)
+  simNet <- net_main(peptideSet)
   simNet_PairsDF <- net_pairs_DF(simNet, forceMutType=forceMutType)
-  if(is.null(numSet)){
-    return(list("SimilarityNetwork"=simNet, "PairsDF"=simNet_PairsDF))
-  }
+  if(is.null(numSet)) return(list("SimilarityNetwork"=simNet, "PairsDF"=simNet_PairsDF))
 
   # A directional, weighted network
-  df_num <- data.frame("Peptide"=as.character(aaStringSet), "Score"=numSet, stringsAsFactors=F)
-  simNet_PairsDF_Directional <- simNet_PairsDF %>%
+  df_num <- data.frame("Peptide"=as.character(peptideSet), "Score"=numSet, stringsAsFactors=F)
+  simNet_PairsDF_DW <- simNet_PairsDF %>%
     dplyr::inner_join(df_num, by=c("AASeq1"="Peptide")) %>%
     dplyr::inner_join(df_num, by=c("AASeq2"="Peptide")) %>%
     dplyr::transmute(AASeq1, AASeq2, Score1=Score.x, Score2=Score.y, MutType, MutPattern)
   mutType_Rev <- function(mutType){
     paste0(lapply(strsplit(mutType, "|", fixed=T)[[1]], function(mut){stringi::stri_reverse(mut)}), collapse="|")
   }
-  simNet_PairsDF_Directional <- dplyr::bind_rows(
-    simNet_PairsDF_Directional,
-    simNet_PairsDF_Directional %>%
+  simNet_PairsDF_DW <- dplyr::bind_rows(
+    simNet_PairsDF_DW,
+    simNet_PairsDF_DW %>%
       dplyr::rename(AASeq1=AASeq2, AASeq2=AASeq1, Score1=Score2, Score2=Score1) %>%
       dplyr::mutate(MutType=sapply(MutType, mutType_Rev))
-  ) %>% 
+  ) %>%
     dplyr::filter(Score1>=Score2) %>%
     dplyr::transmute(AASeq1, AASeq2, Score1, Score2, DeltaScore=Score1-Score2, MutType, MutPattern)
-  simNet_Directional <- simNet_PairsDF_Directional %>%
-    igraph::graph_from_data_frame(directed=T)
-  simNet_Directional <- simNet_Directional %>%
-    igraph::set_vertex_attr("label", value=igraph::V(simNet_Directional)$name)
-  if(weighted==T) igraph::E(simNet_Directional)$weight <- igraph::E(simNet_Directional)$DeltaScore
+  simNet_DW <- igraph::graph_from_data_frame(simNet_PairsDF_DW, directed=directed)
+  simNet_DW <- igraph::set_vertex_attr(simNet_DW, name="label", value=igraph::V(simNet_DW)$name)
+  if(weighted==T) igraph::E(simNet_DW)$weight <- igraph::E(simNet_DW)$DeltaScore
   return(list("SimilarityNetwork"=simNet, "PairsDF"=simNet_PairsDF,
-              "SimilarityNetwork_Directional"=simNet_Directional, "PairsDF_Directional"=simNet_PairsDF_Directional))
+              "SimilarityNetwork_DW"=simNet_DW, "PairsDF_DW"=simNet_PairsDF_DW))
 }
