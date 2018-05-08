73bede8d43000edf33e938c7e2d967eeec007851
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2018-05-08 15:58:15 +0900

    Delete Dataset_EpitopeDataset_Primates.R

--- R/Dataset_EpitopeDataset_Primates.R
+++
@@ -1,8 +0,0 @@
-#' A compiled dataset of primate MHC class I epitopes and ligands.
-#'
-#' @format A tibble with 411 rows and 7 variables
-#' @docType data
-#' @keywords datasets
-#' @name Dataset_EpitopeDataset_Primates
-#' @rdname Dataset_EpitopeDataset_Primates
-"EpitopeDataset_Primates"
