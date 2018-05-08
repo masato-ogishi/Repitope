cf600262c9e6b1fd4ba66f697c55eb47ef6261a1
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2018-05-08 15:58:08 +0900

    Delete Dataset_EpitopeDataset_Mouse.R

--- R/Dataset_EpitopeDataset_Mouse.R
+++
@@ -1,8 +0,0 @@
-#' A compiled dataset of mouse MHC class I epitopes and ligands.
-#'
-#' @format A tibble with 8693 rows and 7 variables
-#' @docType data
-#' @keywords datasets
-#' @name Dataset_EpitopeDataset_Mouse
-#' @rdname Dataset_EpitopeDataset_Mouse
-"EpitopeDataset_Mouse"
